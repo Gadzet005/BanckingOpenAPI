af41367a29a289699b85d6dc09e6da2e057c1d75
Aleksandr <aleks.safarov2005@gmail.com>
2024-11-01 11:40:55 +0300

    add auth by email

--- backend/src/users/serializers.py
+++ backend/src/users/serializers.py
@@ -1,56 +1,57 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
+
 
 User = get_user_model()
 
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password]
-    )
-    phone_number = serializers.CharField(required=True, max_length=15)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("phone_number", "password")
+        fields = ('email', 'phone_number', 'password')
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Пользователь с таким адресом электронной почты уже существует.")
+        return value
 
     def validate_phone_number(self, value):
-        """Check if the phone number is already in use."""
         if User.objects.filter(phone_number=value).exists():
             raise serializers.ValidationError("Пользователь с таким номером телефона уже существует.")
         return value
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data["phone_number"],
-            phone_number=validated_data["phone_number"],
+            email=validated_data['email'],
+            phone_number=validated_data['phone_number'],
         )
-        user.set_password(validated_data["password"])
+        user.set_password(validated_data['password'])
         user.save()
         return user
 
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    phone_number = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
     def validate(self, attrs):
-        phone_number = attrs.get('phone_number')
+        email = attrs.get('email')
         password = attrs.get('password')
 
-        user = authenticate(phone_number=phone_number, password=password)
+        user = authenticate(email=email, password=password)
 
         if user is None:
-            raise serializers.ValidationError(
-                _("Не удалось войти с предоставленными данными.")
-            )
+            raise serializers.ValidationError(_("Не удалось войти с предоставленными данными."))
 
         if not user.is_active:
             raise serializers.ValidationError(_("Пользователь неактивен."))
 
-        data = super().validate(attrs)
+        data = super().validate({'email': user.email, 'password': password})
         return data
