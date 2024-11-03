20bd098125d96d552d85cf78d0553398df784e92
Aleksandr <aleks.safarov2005@gmail.com>
2024-11-03 20:19:27 +0300

    time zone fix

--- backend/src/conf/settings.py
+++ backend/src/conf/settings.py
@@ -138,11 +138,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "ru"
 
-TIME_ZONE = "Europe/Moscow"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
