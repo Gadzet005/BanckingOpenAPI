fd614061b7b2a13cb9f044ffa6bb07724804920e
Aleksandr <aleks.safarov2005@gmail.com>
2024-11-03 15:25:31 +0300

    fix get all trnasactions

--- backend/src/api/serializers.py
+++ backend/src/api/serializers.py
@@ -4,7 +4,8 @@ from banking.models import Transaction, Account, Bank
 class TransactionSerializer(serializers.ModelSerializer):
     account_code = serializers.IntegerField(source='account_id.account_code', read_only=True)
     bank_name = serializers.CharField(source='account_id.bank_id.name', read_only=True)
+    bank_code = serializers.IntegerField(source='account_id.bank_id.bank_code', read_only=True)
 
     class Meta:
         model = Transaction
-        fields = ['id', 'amount', 'type', 'description', 'date', 'account_code', 'bank_name']
+        fields = ['id', 'amount', 'type', 'description', 'date', 'account_code', 'bank_name', 'bank_code']
