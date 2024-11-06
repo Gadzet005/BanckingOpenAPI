fb5bd229d34b645ed990bd127a9bb1f1f277823d
ekulp <e.kulpin@mail.ru>
2024-11-06 18:51:58 +0300

    Tested + Minor changes

--- backend/mock_bank/bank/urls.py
+++ backend/mock_bank/bank/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import MakeTransaction
+from .views import (MakeTransaction,
+                    AuthView,
+                    SubscribeView,
+                    UnsubscribeView,
+                    AccountInfoView)
 
 urlpatterns = [
     path(
@@ -7,4 +11,24 @@ urlpatterns = [
         MakeTransaction.as_view(),
         name='make_transaction'
     ),
+    path(
+        'get_token/',
+        AuthView.as_view(),
+        name='get_token'
+    ),
+    path(
+        'subscribe/',
+        SubscribeView.as_view(),
+        name='subscribe'
+    ),
+    path(
+        'unsubscribe/',
+        UnsubscribeView.as_view(),
+        name='unsubscribe'
+    ),
+    path(
+        'getinfo/',
+        AccountInfoView.as_view(),
+        name='getinfo'
+    )
 ]
