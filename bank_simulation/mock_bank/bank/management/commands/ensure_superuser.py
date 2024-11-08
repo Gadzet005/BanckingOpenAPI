ceb75a9bcf114e2a711cbe6d74f2d67f2a1d4dea
Gadzet005 <88264036+Gadzet005@users.noreply.github.com>
2024-11-08 14:58:19 +0300

    фикс создания superuser (#33)

--- bank_simulation/mock_bank/bank/management/commands/ensure_superuser.py
+++ bank_simulation/mock_bank/bank/management/commands/ensure_superuser.py
@@ -5,15 +5,9 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        username = os.getenv("DJANGO_SUPERUSER_NAME")
-        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-
-        if not username or not password or not email:
-            raise CommandError(
-                "DJANGO_SUPERUSER_NAME, DJANGO_SUPERUSER_PASSWORD, and DJANGO_SUPERUSER_EMAIL"
-                "environment variables must be set."
-            )
+        username = os.getenv("DJANGO_SUPERUSER_NAME", "admin")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "1234")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@admin.com")
 
         try:
             User.objects.create_superuser(username, email, password)
